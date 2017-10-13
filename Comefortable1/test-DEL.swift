f4fc11d5e0213d2a2741ee846c580a98a47292b8
yhazir2017 <yildiray.hazir@gmail.com>
2017-10-13 11:09:19 -0700

    ok

---
+++ Comefortable1/test-DEL.swift
@@ -0,0 +1,20 @@
+//
+//  test.swift
+//  Comefortable1
+//
+//  Created by YILDIRAY HAZIR on 5/16/17.
+//  Copyright © 2017 codepath. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class test : UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named:"bckgrnd1.jpg")!)
+}
+}
