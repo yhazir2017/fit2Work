fad3fba4a1c06afdb72dfd48fe783c36cf1abe00
yhazir2017 <yildiray.hazir@gmail.com>
2017-10-13 11:08:56 -0700

    ok

---
+++ Exercise-DEL.swift
@@ -0,0 +1,20 @@
+//
+//  Exercise.swift
+//  Comefortable1
+//
+//  Created by YILDIRAY HAZIR on 4/18/17.
+//  Copyright © 2017 codepath. All rights reserved.
+//
+import Foundation
+
+class Exercise {
+    var type = ""
+    var image = ""
+    var text = ""
+    
+    init(type: String,image: String,text: String) {
+        self.type = type
+        self.image = image
+        self.text = text
+    }
+}
